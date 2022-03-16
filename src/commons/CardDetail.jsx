553fe17d32c2302670585d490e0d78490c250eac
NicolasRequejo <nicolasrequejo.96@gmail.com>
2022-03-16 10:28:38 -0300

    Algunos retoques

--- src/commons/CardDetail.jsx
+++ src/commons/CardDetail.jsx
@@ -1,13 +1,28 @@
 import "../styles/cardetail.css";
 import { ProductContext } from "../context/product";
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import ButtonSize from "./ButtonSize";
 import userReducer from "../states/usario";
+import { useDispatch, useSelector } from "react-redux";
+import { addCartItem } from "../states/cart";
+import { SizeContext } from "../context/size";
+import { useParams } from "react-router";
 
 const CardDetail = () => {
   const { producto, setProducto } = useContext(ProductContext);
+  const { size } = useContext(SizeContext);
+  const [cantidad, setCantidad] = useState(0);
+  const id = useParams("id");
 
-  return (
+  const dispatch = useDispatch();
+
+  const sendCart = () => {
+    dispatch(
+      addCartItem({ productId: producto.id, cantidad: cantidad, talle: size })
+    );
+  };
+
+  return producto.inventarios ? (
     <div className="cardetail">
       <div className="foto">
         <img className="fotito" src={producto.image_url} alt="" />
@@ -16,20 +31,38 @@ const CardDetail = () => {
         <div className="nose">
           <span>{producto.marca}</span>
           <h1>{producto.modelo}</h1>
-          <div className="gridButton">
-            {producto.inventarios.map((t, i) => (
-              <ButtonSize key={i} talle={t.talle} />
-            ))}
+          <div>
+            <div>
+              <strong>Tabla de talles</strong>
+            </div>
+            <div className="gridButton">
+              {producto.inventarios.map((t, i) => (
+                <ButtonSize key={i} talle={t.talle} />
+              ))}
+            </div>
           </div>
 
           <div>
             <h3>Precio ${producto.price}</h3>
-            <button className="boton"> Añadir al Carrito</button>
+            <button className="boton" onClick={sendCart}>
+              {" "}
+              Añadir al Carrito
+            </button>
+            <label>
+              Cantidad:
+              <input
+                type="number"
+                className="cantidad"
+                onChange={(e) => setCantidad(e.target.value)}
+              />
+            </label>
           </div>
           <p>{producto.descripcion}</p>
         </div>
       </div>
     </div>
+  ) : (
+    <h1 className="mt-7 is-size-1 has-text-centered"> Cargando aguanta ...</h1>
   );
 };
 
